5177630724d34a72c1e444604dc04a1c051b022b
jlbuild <juliabuildbot@gmail.com>
2022-10-14 09:10:12 +0000

    CUDA_Runtime_jll build 0.2.0+0

--- src/wrappers/x86_64-linux-gnu-cuda+11.8.jl
+++ src/wrappers/x86_64-linux-gnu-cuda+11.8.jl
@@ -9,12 +9,12 @@ JLLWrappers.@declare_library_product(libcupti, "libcupti.so.11.8")
 JLLWrappers.@declare_library_product(libcurand, "libcurand.so.10")
 JLLWrappers.@declare_library_product(libcusolver, "libcusolver.so.11")
 JLLWrappers.@declare_library_product(libcusolverMg, "libcusolverMg.so.11")
+JLLWrappers.@declare_library_product(libcusparse, "libcusparse.so.11")
 JLLWrappers.@declare_file_product(libdevice)
 JLLWrappers.@declare_library_product(libnvtoolsext, "libnvToolsExt.so.1")
 JLLWrappers.@declare_library_product(libnvvm, "libnvvm.so.4")
 JLLWrappers.@declare_executable_product(compute_sanitizer)
 JLLWrappers.@declare_file_product(libcudadevrt)
-JLLWrappers.@declare_library_product(libcusparse, "libcusparse.so.11")
 JLLWrappers.@declare_executable_product(nvdisasm)
 JLLWrappers.@declare_executable_product(nvlink)
 JLLWrappers.@declare_executable_product(ptxas)
@@ -62,6 +62,12 @@ function __init__()
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
+    JLLWrappers.@init_library_product(
+        libcusparse,
+        "lib/libcusparse.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
     JLLWrappers.@init_file_product(
         libdevice,
         "share/libdevice/libdevice.10.bc",
@@ -87,12 +93,6 @@ function __init__()
     JLLWrappers.@init_file_product(
         libcudadevrt,
         "lib/libcudadevrt.a",
-    )
-
-    JLLWrappers.@init_library_product(
-        libcusparse,
-        "lib/libcusparse.so",
-        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_executable_product(
