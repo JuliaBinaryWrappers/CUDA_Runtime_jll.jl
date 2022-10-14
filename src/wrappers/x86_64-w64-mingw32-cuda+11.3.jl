7958f5da6244c85dbccad60f646143e1ddfccf35
jlbuild <juliabuildbot@gmail.com>
2022-10-14 10:34:18 +0000

    CUDA_Runtime_jll build 0.2.0+0

--- src/wrappers/x86_64-w64-mingw32-cuda+11.3.jl
+++ src/wrappers/x86_64-w64-mingw32-cuda+11.3.jl
@@ -6,15 +6,15 @@ JLLWrappers.@declare_library_product(libcublas, "cublas64_11.dll")
 JLLWrappers.@declare_library_product(libcudart, "cudart64_110.dll")
 JLLWrappers.@declare_library_product(libcufft, "cufft64_10.dll")
 JLLWrappers.@declare_library_product(libcupti, "cupti64_2021.1.1.dll")
-JLLWrappers.@declare_library_product(libcusolver, "cusolver64_11.dll")
+JLLWrappers.@declare_library_product(libcurand, "curand64_10.dll")
 JLLWrappers.@declare_library_product(libcusolverMg, "cusolverMg64_11.dll")
 JLLWrappers.@declare_library_product(libcusparse, "cusparse64_11.dll")
+JLLWrappers.@declare_file_product(libdevice)
 JLLWrappers.@declare_library_product(libnvtoolsext, "nvToolsExt64_1.dll")
 JLLWrappers.@declare_library_product(libnvvm, "nvvm64_40_0.dll")
 JLLWrappers.@declare_executable_product(compute_sanitizer)
 JLLWrappers.@declare_file_product(libcudadevrt)
-JLLWrappers.@declare_library_product(libcurand, "curand64_10.dll")
-JLLWrappers.@declare_file_product(libdevice)
+JLLWrappers.@declare_library_product(libcusolver, "cusolver64_11.dll")
 JLLWrappers.@declare_executable_product(nvdisasm)
 JLLWrappers.@declare_executable_product(nvlink)
 JLLWrappers.@declare_executable_product(ptxas)
@@ -45,8 +45,8 @@ function __init__()
     )
 
     JLLWrappers.@init_library_product(
-        libcusolver,
-        "bin\\cusolver64_11.dll",
+        libcurand,
+        "bin\\curand64_10.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
@@ -60,6 +60,11 @@ function __init__()
         libcusparse,
         "bin\\cusparse64_11.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_file_product(
+        libdevice,
+        "share\\libdevice\\libdevice.10.bc",
     )
 
     JLLWrappers.@init_library_product(
@@ -85,14 +90,9 @@ function __init__()
     )
 
     JLLWrappers.@init_library_product(
-        libcurand,
-        "bin\\curand64_10.dll",
+        libcusolver,
+        "bin\\cusolver64_11.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
-    )
-
-    JLLWrappers.@init_file_product(
-        libdevice,
-        "share\\libdevice\\libdevice.10.bc",
     )
 
     JLLWrappers.@init_executable_product(
