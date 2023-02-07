5017e1d6b8f7b5f8f06f183271203d6089cd11bc
jlbuild <juliabuildbot@gmail.com>
2023-02-07 15:23:23 +0000

    CUDA_Runtime_jll build 0.3.1+0

--- src/wrappers/powerpc64le-linux-gnu-cuda+11.0.jl
+++ src/wrappers/powerpc64le-linux-gnu-cuda+11.0.jl
@@ -12,9 +12,9 @@ JLLWrappers.@declare_library_product(libcurand, "libcurand.so.10")
 JLLWrappers.@declare_library_product(libcusolver, "libcusolver.so.10")
 JLLWrappers.@declare_library_product(libcusolverMg, "libcusolverMg.so.10")
 JLLWrappers.@declare_library_product(libcusparse, "libcusparse.so.11")
+JLLWrappers.@declare_executable_product(compute_sanitizer)
 JLLWrappers.@declare_file_product(libdevice)
 JLLWrappers.@declare_library_product(libnvvm, "libnvvm.so.3")
-JLLWrappers.@declare_executable_product(compute_sanitizer)
 JLLWrappers.@declare_executable_product(nvdisasm)
 JLLWrappers.@declare_executable_product(nvlink)
 JLLWrappers.@declare_executable_product(ptxas)
@@ -73,6 +73,11 @@ function __init__()
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
+    JLLWrappers.@init_executable_product(
+        compute_sanitizer,
+        "bin/compute-sanitizer",
+    )
+
     JLLWrappers.@init_file_product(
         libdevice,
         "share/libdevice/libdevice.10.bc",
@@ -82,11 +87,6 @@ function __init__()
         libnvvm,
         "lib/libnvvm.so",
         RTLD_LAZY | RTLD_DEEPBIND,
-    )
-
-    JLLWrappers.@init_executable_product(
-        compute_sanitizer,
-        "bin/compute-sanitizer",
     )
 
     JLLWrappers.@init_executable_product(
--- src/wrappers/x86_64-w64-mingw32-cuda+11.8.jl
+++ src/wrappers/x86_64-w64-mingw32-cuda+11.8.jl
@@ -12,9 +12,9 @@ JLLWrappers.@declare_library_product(libcurand, "curand64_10.dll")
 JLLWrappers.@declare_library_product(libcusolver, "cusolver64_11.dll")
 JLLWrappers.@declare_library_product(libcusolverMg, "cusolverMg64_11.dll")
 JLLWrappers.@declare_library_product(libcusparse, "cusparse64_11.dll")
+JLLWrappers.@declare_file_product(libdevice)
 JLLWrappers.@declare_library_product(libnvvm, "nvvm64_40_0.dll")
 JLLWrappers.@declare_executable_product(compute_sanitizer)
-JLLWrappers.@declare_file_product(libdevice)
 JLLWrappers.@declare_executable_product(nvdisasm)
 JLLWrappers.@declare_executable_product(nvlink)
 JLLWrappers.@declare_executable_product(ptxas)
@@ -73,6 +73,11 @@ function __init__()
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
+    JLLWrappers.@init_file_product(
+        libdevice,
+        "share\\libdevice\\libdevice.10.bc",
+    )
+
     JLLWrappers.@init_library_product(
         libnvvm,
         "bin\\nvvm64_40_0.dll",
@@ -82,11 +87,6 @@ function __init__()
     JLLWrappers.@init_executable_product(
         compute_sanitizer,
         "bin\\compute-sanitizer.exe",
-    )
-
-    JLLWrappers.@init_file_product(
-        libdevice,
-        "share\\libdevice\\libdevice.10.bc",
     )
 
     JLLWrappers.@init_executable_product(
