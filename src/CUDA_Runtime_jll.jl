77ad6b4d518176ece01e11a6895ae8e76619dd62
jlbuild <juliabuildbot@gmail.com>
2026-01-14 09:27:25 +0000

    CUDA_Runtime_jll build 0.20.1+0

--- src/CUDA_Runtime_jll.jl
+++ src/CUDA_Runtime_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CUDA_Runtime")
-JLLWrappers.@generate_main_file("CUDA_Runtime", UUID("76a88914-d11a-5bdc-97e0-2f5a05c973a2"))
+JLLWrappers.@generate_main_file("CUDA_Runtime", Base.UUID("76a88914-d11a-5bdc-97e0-2f5a05c973a2"))
 end  # module CUDA_Runtime_jll
